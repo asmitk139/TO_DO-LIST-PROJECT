5d881c57d60ba8f45b61ae8524d520e93dca4564
Asmit <asmitk139@gmaqil.com>
2025-05-27 23:16:34 +0530

    Day 2

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const bodyparser = require("body-parser");
 var app = express();
 app.set("view engine" , "ejs");
 app.use(express.urlencoded({extended:true}));
-
+var items = [];
 var example = "working";
 app.get("/",function(req,res){
-    res.render("list",{exej : example})
+    res.render("list",{ejes : items})
 });
 app.post("/",function(req,res){
-    console.log(req.body.ele1);
+    var item = req.body.ele1;
+    items.push(item);
+    res.redirect("/");
 });
 
 
